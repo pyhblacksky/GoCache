4227cee89b98c7d4df722629a4692f1607fe73d0
pyh_uestc <664254320@qq.com>
2019-06-24 15:05:56 +0800

    complete get set del method

--- src/cache/cacheImpl.go
+++ src/cache/cacheImpl.go
@@ -4,17 +4,17 @@ import "sync"
 
 //结构： map + 锁保护
 type SimpleCache struct {
-	c 		map[string][]byte
-	mutex 	sync.RWMutex
+	c     map[string][]byte
+	mutex sync.RWMutex
 	Stat
 }
 
 //Set方法
-func (c *SimpleCache) Set(k string, v []byte) error{
-	c.mutex.Lock()		//锁
-	defer c.mutex.Unlock()
+func (c *SimpleCache) Set(k string, v []byte) error {
+	c.mutex.Lock()         //锁
+	defer c.mutex.Unlock() //保证mutex对象锁住后的自动释放
 	tmp, exist := c.c[k]
-	if exist{
+	if exist {
 		c.del(k, tmp)
 	}
 	c.c[k] = v
@@ -22,3 +22,31 @@ func (c *SimpleCache) Set(k string, v []byte) error{
 	return nil
 }
 
+//Get方法
+func (c *SimpleCache) Get(k string) ([]byte, error) {
+	c.mutex.RLock()
+	defer c.mutex.RUnlock()
+	return c.c[k], nil
+}
+
+//Del方法
+func (c *SimpleCache) Del(k string) error {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	v, exist := c.c[k]
+	if exist {
+		delete(c.c, k)
+		c.del(k, v)
+	}
+	return nil
+}
+
+//获取状态
+func (c *SimpleCache) GetStat() Stat {
+	return c.Stat
+}
+
+//在内存中新建新缓存
+func newInMemoryCache() *SimpleCache {
+	return &SimpleCache{make(map[string][]byte), sync.RWMutex{}, Stat{}}
+}
--- src/main.go
+++ src/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"./cache"
+	"fmt"
+)
 
-//主要程序
-
+//主程序
 func main() {
+
+	c := cache.New("inmemory")
+	k, v := "sola", []byte{'a', 'b', 'c', 'd', 'e', 'f'}
+
+	c.Set(k, v)
+	tmp, _ := c.Get(k)
+	fmt.Println("Key:", k, " value: ", tmp)
+
+	c.Del(k)
+	tmp, _ = c.Get(k)
+	fmt.Println("Key:", tmp, " value: ", tmp)
+
 	fmt.Println("hello")
 }
